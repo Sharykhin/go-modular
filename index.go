decc390379eca83f2dba70b427352c23b85ec102
Sharykhin <siarhei.sharykhin@gmail.com>
2015-03-02 23:18:53 +0300

    ISSUE-#2 has been completed. There were added handler of static files

--- index.go
+++ index.go
@@ -9,13 +9,15 @@ import (
 )
 
 func main() {
-	
+
 	var indexController controller.IndexController
 	var postController controller.PostController
 	// one more variant of defining controller
 	adminDefaultController := new(admin.DefaultController)
 	var userDefaultController user.DefaultController
 
+	// Handle static files such as styles and scripts
+	http.Handle("/public/vendor/",http.StripPrefix("/public/vendor/",http.FileServer(http.Dir("public/vendor"))))
 
 	http.HandleFunc("/", indexController.IndexAction)
 	http.HandleFunc("/posts", postController.IndexAction)
